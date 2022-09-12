10af882c11baafd5f490b909c2d42cd72fded35d
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-12 20:38:18 -0300

    variaveis_constantes

---
+++ Curso-DIO-Python/variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = "Raul"
+idade = 27
+
+nome = "Fidelis"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+limiteSaqueDiario = 1000
+
+BRASIL_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRASIL_STATES)
