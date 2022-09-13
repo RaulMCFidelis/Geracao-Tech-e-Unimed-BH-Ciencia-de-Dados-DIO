2c3b88fe0ec3ee6f7ef9a3fd26c2e257a10cbed7
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-13 06:22:19 -0300

    operadores-de-atribuição

---
+++ Curso-DIO-Python/operadores_atribuicao.py
@@ -0,0 +1,45 @@
+# Atribuição simple
+saldo = 500
+print(saldo)
+#500
+
+# Atribuição com adição
+saldo = 500
+saldo += 200
+print(saldo)1
+#700
+
+# Atribuição com subtração
+saldo = 500
+saldo -= 100
+print(saldo)
+#400
+
+# Atribuição com multiplicação
+saldo = 500
+saldo *= 2
+print(saldo)
+#1000
+
+# Atribuição com divisão
+saldo = 500
+saldo //= 5
+print(saldo)
+#100
+
+saldo = 500
+saldo /= 5
+print(saldo)
+#100.0
+
+# Atribuição com módulo
+saldo = 500
+saldo %= 480
+print(saldo)
+#20
+
+# Atribuição com exponenciação 
+saldo = 500
+saldo **= 2
+#2500
+
