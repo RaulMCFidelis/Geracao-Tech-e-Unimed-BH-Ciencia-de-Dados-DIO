5cfc3a26bea81a8f863edb8f34bb6b475e98180d
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-13 06:03:56 -0300

    operadores-de-comparação

---
+++ Curso-DIO-Python/operadores_comparacao.py
@@ -0,0 +1,20 @@
+saldo = 450
+saque = 200
+print(saldo == saque)
+#False
+
+print(saldo != saque) # != diferença 
+#True
+
+print(saldo > saque)
+#True
+
+print(saldo >= saque)
+#True
+
+print(saldo < saque)
+#False
+
+print(saldo <= saque)
+#False
+
