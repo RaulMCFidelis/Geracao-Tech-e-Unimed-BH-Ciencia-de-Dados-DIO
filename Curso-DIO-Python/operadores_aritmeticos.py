fd28743acdb637a18a0d65175f1efdc8c20df863
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-13 05:53:38 -0300

    operadores-aritméticos

---
+++ Curso-DIO-Python/operadores_aritmeticos.py
@@ -0,0 +1,57 @@
+# Adição
+print(1 + 1)
+
+# Subtração
+print(10 - 2)
+
+# Multiplicação
+print(4 * 3)
+
+# Divisão
+print(12 / 3)
+
+#Divisão inteira
+print(12 // 2)
+
+# Módulo (resto)
+print(10 % 3)
+
+#Exponenciação
+print(2 ** 3)
+
+print(10 - 5 * 2)
+
+print((10-5)*2)
+
+print(10 ** 2 * 2)
+
+print(10 ** (2 * 2))
+
+print(10 / 2 * 4)
+
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 + produto_2 + 3.5)
+print(produto_1 - produto_2)
+print(produto_2 / produto_1)
+print(produto_2 // produto_1)
+print(produto_2 * produto_1)
+print(produto_2 % produto_1)
+print(produto_2 ** produto_1)
+
+x = 10 + 5 * 4
+print(x)
+
+y = (10 + 5) * 4
+print(y)
+
+z = 10 /2 + 25 * 2 - 2 ** 2
+z = (10 / 2) + (25 * 2) - (2 ** 2) # Esquerda para a direita () boa pratica
+print(z)
+
+h = (10 / 2) + 25 * ((2 - 2) ** 2)
+print(h)
+
+
+
